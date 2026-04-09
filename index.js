740e355f8ee0e7ebcf8518385617e65969185b11
HuzaifaaM007 <huzaifamurtaza007@gmail.com>
2026-04-09 11:11:51 +0500

    Fixed package.json

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.write('Hello from DevOps CI/CD Pipeline!');
+  res.end();
+});
 
+server.listen(80, () => {
+  console.log('Server running on port 80');
+});
